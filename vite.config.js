a59242d03771f99ff198e3c53dcf4f188cc751ca
Digesh123 <digeshghevade123@gmail.com>
2025-04-11 12:38:47 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/hamgrp/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base:"/hem_1/"
 })
